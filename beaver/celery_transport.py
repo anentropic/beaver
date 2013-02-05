800c2daf6c659dcc393ae9822f7b86cadd550f98
Paul Garner <ego@anentropic.com>
2013-02-05 13:12:42 +0000

    small help for testing

--- beaver/celery_transport.py
+++ beaver/celery_transport.py
@@ -1,15 +1,15 @@
-import celery
-
 from beaver.transport import Transport
 
 
 class CeleryTransport(Transport):
     # need to supply a string (dotted import path) or celery task object here:
     celery_task = None
+    # your instantiated Celery() app (required to use string above)
+    celery_app = None
 
     def callback(self, filename, lines):
-        if isinstance(self.celery_task, basestring):
-            task_method = partial(celery.send_task, self.celery_task)
+        if isinstance(self.celery_task, basestring) and self.celery_app is not None:
+            task_method = partial(self.celery_app.send_task, self.celery_task)
         else:
             try:
                 task_method = getattr(self.celery_task, 'apply_async')
@@ -17,4 +17,5 @@ class CeleryTransport(Transport):
                 raise NotImplemented('You must supply a Celery task object')
 
         lines = [self.format(filename, line) for line in lines]
-        task_method(args=[lines])
+        # beaver ignores return values but it helps for testing
+        return task_method(args=[lines])
